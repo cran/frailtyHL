924c586ff34fcfe1f44ff96e8809c6797801033f
Maengseok Noh <msnoh@pknu.ac.kr>
2019-11-22 08:00:05 +0000

    version 2.3

--- R/mlmfit.R
+++ R/mlmfit.R
@@ -1,7 +1,14 @@
 mlmfit <-
-function(jm1,data_surv,Maxiter=200) {
+function(jm1,data, weights, subset, na.action,Maxiter=200) {
+    Call <- match.call()
+    mc <- match.call()
+    indx <- match(c("data", "weights", "subset", "na.action"), 
+        names(Call), nomatch = 0)
+    if (indx[1] == 0) 
+        stop("A formula argument is required")
   res1<-design_frailtyHL(jm1[[3]],data=data)
   kkk<-length(res1[[4]])
+  data_surv<-data
   if (length(res1[[4]])==1) res<-mlmfit1(jm1=jm1,data_surv,Maxiter=Maxiter)
   if (length(res1[[4]])==2) res<-mlmfit2(jm1=jm1,data_surv,Maxiter=Maxiter)
   return(res)
